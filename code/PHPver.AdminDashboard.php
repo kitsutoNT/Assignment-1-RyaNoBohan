2b7b5bbeee3962efd391513b6c4de2c92f70d84d
KitsutoNT <ntaka520@mtroyal.ca>
2016-02-08 20:15:35 +0000

    started to create a php version of admin dashboard page creted visits-head-inc.php that include necessary references

---
+++ code/PHPver.AdminDashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include "lib/includes/visits-head.inc.php"; ?>
+    </head>
+    <body>
+        <h1>Fuck</h1>
+    </body>
+</html>
---
+++ code/lib/includes/visits-head.inc.php
@@ -0,0 +1,18 @@
+<meta charset="UTF-8">
+<meta name="description" content="">
+<meta name="author" content="Ryan Barton, Bohan Yao, Nobufumi Takahashi">
+<meta name="title" content="WebIII-Assignment1">
+
+<!--Material Design font icon stylesheet
+Roboto, which is the Material Design standard font
+The main stylesheet
+The JavaScript file-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+<link rel="stylesheet" href="https://code.getmdl.io/1.1.1/material.cyan-light_blue.min.css" />
+<script defer src="https://code.getmdl.io/1.1.1/material.min.js"></script>
+
+<!--JQuery library using google CDN-->
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
+
+<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
